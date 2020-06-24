8d915590b60def72e7f24b9424845f75330731d0
Welder Mileu <welderfelix21@gmail.com>
2020-06-24 03:58:58 -0300

    Setando o cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express    = require('express')
 const bodyParser = require('body-parser')
 const router     = require('./routes')
+const cors       = require('cors')
 const app        = express()
 
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
